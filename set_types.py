f1ceacc137f4066925b9c61c472d7565e013caa0
Reks9705 <32132690+Reks9705@users.noreply.github.com>
2022-12-06 18:57:01 +0200

    Add files via upload
    Upload class activity /sets, bools/ 06.12

---
+++ set_types.py
@@ -0,0 +1,38 @@
+import decimal
+
+a = {2, 3, 4}
+b = {2, 5, 6}
+
+
+print(a & b, "a & b")
+print(a | b, "a | b")
+print(a ^ b, "a ^ b")
+print(a - b, "a - b")
+print(a == b, "a == b")
+print(a != b, "a != b")
+print(a <= b, "a <= b")
+print(a < b, "a < b")
+print(a >= b, "a >=b")
+
+(not(True and True)) == ((not True) or (not True)) # сиг всичко е труе
+(not(True and False)) == ((not True) or (not False))
+(not(False and True)) == ((not False) or (not True))
+(not(False and False)) == ((not False) or (not False))
+
+(not(True or True)) == ((not True) and (not True))
+(not(True or False)) == ((not True) and (not False))
+(not(False or True)) == ((not False) and (not True))
+(not(False or False)) == ((not False) and (not False))
+
+
+name = "Galin"
+age = 11
+median =
+percent = 99.00
+student_names = "Spiridon Spiridonov"
+coordinates = decimal.Decimal("11111.22222")
+map_of_coordinates = (111.111, 111.222)
+students_data =  {"age": "11", "name" : "Spiridon"}
+unique_ages = {"11","12","13"}
+binary_name =
+
